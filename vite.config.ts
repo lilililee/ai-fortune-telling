ee4f911ddcfb40af3530c4eaec36133973be8c43
liguang <liguang@webull.com>
2026-01-28 14:44:52 +0800

    feat: 调整 build 配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
   resolve: {
     alias: {
